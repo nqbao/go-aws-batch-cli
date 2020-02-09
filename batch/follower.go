4c3f7854a449a2d18e502db001e92b870eba0415
Bao Nguyen <b@nqbao.com>
2020-02-09 13:19:51 -0800

    Find the running container of the batch job

--- batch/follower.go
+++ batch/follower.go
@@ -1,7 +1,9 @@
 package batch
 
 import (
+	"fmt"
 	"io"
+	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -23,7 +25,6 @@ func (b *BatchCli) FollowJob(jobID string) *JobFollower {
 
 	go func() {
 		prevStatus := ""
-		attempts := 0
 		var logFollower *LogFollower
 
 		for {
@@ -38,40 +39,26 @@ func (b *BatchCli) FollowJob(jobID string) *JobFollower {
 				// RUNNABLE -> STARTING -> RUNNING
 				if newStatus != prevStatus {
 					follower.Status <- *job.Status
-				}
 
-				if len(job.Attempts) > attempts {
-					attempts = len(job.Attempts)
+					if newStatus == "RUNNING" {
+						runningContainer, err := b.GetRunningContainer(jobID)
 
-					go func(stream string) {
-						// fmt.Printf(">> Start log stream %v\n", stream)
-
-						ff := FollowCloudWatchLog(b.Session, "/aws/batch/job", stream)
-
-						for {
-							select {
-							case msg := <-ff.Out:
-								if msg != "" {
-									follower.Logging <- msg
-								}
-
-							case err := <-ff.Error:
-								if err == io.EOF {
-									// fmt.Printf(">> Log stream %v end\n", stream)
-								} else {
-									// fmt.Printf(">> Log stream error %v\n", err)
-									// TODO: propagate to job follower?
-								}
-
-								break
-							}
+						if err != nil {
+							follower.Error <- fmt.Errorf("Unable to locate running container: %v", err)
+							break
 						}
-					}(*job.Attempts[attempts-1].Container.LogStreamName)
 
-					// we have new attempt, make sure we have drain out
-					// all the previous logs before starting new one
-					if logFollower != nil {
+						bits := strings.Split(*job.JobDefinition, "/")
+						bits = strings.Split(bits[len(bits)-1], ":")
+						streamName := fmt.Sprintf("%v/%v", bits[0], runningContainer)
 
+						go func(streamName string) {
+							if logFollower != nil {
+								logFollower.Terminate <- true
+							}
+
+							logFollower = b.followRunningJobStream(follower, streamName)
+						}(streamName)
 					}
 				}
 
@@ -95,4 +82,33 @@ func (b *BatchCli) FollowJob(jobID string) *JobFollower {
 	}()
 
 	return follower
+}
+
+func (b *BatchCli) followRunningJobStream(follower *JobFollower, stream string) *LogFollower {
+	fmt.Printf(">> Start log stream %v\n", stream)
+
+	ff := FollowCloudWatchLog(b.Session, "/aws/batch/job", stream)
+
+	go func() {
+		for {
+			select {
+			case msg := <-ff.Out:
+				if msg != "" {
+					follower.Logging <- msg
+				}
+
+			case err := <-ff.Error:
+				if err == io.EOF {
+					fmt.Printf(">> Log stream %v end\n", stream)
+				} else {
+					fmt.Printf(">> Log stream error %v\n", err)
+					follower.Error <- err
+				}
+
+				break
+			}
+		}
+	}()
+
+	return ff
 }
--- batch/job.go
+++ batch/job.go
@@ -2,9 +2,12 @@ package batch
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/batch"
+	"github.com/aws/aws-sdk-go/service/ecs"
 )
 
 func (b *BatchCli) GetJob(jobID string) (*batch.JobDetail, error) {
@@ -21,4 +24,75 @@ func (b *BatchCli) GetJob(jobID string) (*batch.JobDetail, error) {
 
 	job := out.Jobs[0]
 	return job, nil
+}
+
+func (b *BatchCli) GetRunningContainer(jobID string) (string, error) {
+	ecsSvc := ecs.New(b.Session)
+	clusters := []string{}
+
+	err := ecsSvc.ListClustersPages(&ecs.ListClustersInput{}, func(out *ecs.ListClustersOutput, last bool) bool {
+		for _, arn := range out.ClusterArns {
+			clusters = append(clusters, *arn)
+		}
+		return true
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	// try to search for container in all of the running containers
+	containerName := ""
+	for _, cluster := range clusters {
+		var nestedError error
+		err = ecsSvc.ListTasksPages(&ecs.ListTasksInput{
+			Cluster:    aws.String(cluster),
+			MaxResults: aws.Int64(50),
+		}, func(out *ecs.ListTasksOutput, last bool) bool {
+			if len(out.TaskArns) > 0 {
+				tasks, err := ecsSvc.DescribeTasks(&ecs.DescribeTasksInput{
+					Cluster: aws.String(cluster),
+					Tasks:   out.TaskArns,
+				})
+
+				if err != nil {
+					nestedError = err
+					return false
+				}
+
+				for _, task := range tasks.Tasks {
+					containerJobID := ""
+
+					// only 1 container is running for now
+					for _, ev := range task.Overrides.ContainerOverrides[0].Environment {
+						if *ev.Name == "AWS_BATCH_JOB_ID" {
+							containerName = *task.Containers[0].Name
+							containerJobID = *ev.Value
+							break
+						}
+					}
+
+					if containerJobID == jobID {
+						bits := strings.Split(*task.TaskArn, "/")
+						containerName = fmt.Sprintf("%v/%v", containerName, bits[len(bits)-1])
+						return false
+					}
+				}
+			}
+
+			return true
+		})
+
+		if err != nil {
+			return "", err
+		} else if nestedError != nil {
+			return "", nestedError
+		}
+
+		if containerName != "" {
+			break
+		}
+	}
+
+	return containerName, nil
 }
